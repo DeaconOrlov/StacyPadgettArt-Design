c4665bf45e1d12489e496253b501334d0aca9b5f
DeaconOrlov <deaconorlov@gmail.com>
2021-07-18 16:04:37 -0400

    add scripting for mobile menu and add menu to all pages

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,5 @@
-//Code to exist shortly
+function toggleMenu() {
+    document.getElementById("topNav").classList.toggle("open");
+}
+const btn = document.getElementById('hamburgerBtn');
+btn.onclick = toggleMenu;
