d0ddf7633f168464339a68cc804212e244f82382
DeaconOrlov <deaconorlov@gmail.com>
2021-07-26 17:35:16 -0400

    Add email inout field to footer further clean up syling

--- js/scripts.js
+++ js/scripts.js
@@ -4,21 +4,19 @@ function toggleMenu() {
 const btn = document.getElementById('hamburgerBtn');
 btn.onclick = toggleMenu;
 
-function ValidateEmail(inputText)
-{
+function ValidateEmail(inputText) {
 const mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-if(inputText.value.match(mailformat))
-{
-alert("Valid email address!");
-document.commentForm.eAddress.focus();
-return true;
-}
-else
-{
-alert("You have entered an invalid email address!");
-document.commentForm.eAddress.focus();
-return false;
-}
+
+    if(inputText.value.match(mailformat)) {
+        alert("Valid email address!");
+        document.commentForm.eAddress.focus();
+        return true;
+    }
+    else {
+        alert("You have entered an invalid email address!");
+        document.commentForm.eAddress.focus();
+        return false;
+    }
 }
 
 function ToggleComment() {
