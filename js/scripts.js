79b4acd526871bb8ec843d708ea003cf028ca441
DeaconOrlov <46255615+DeaconOrlov@users.noreply.github.com>
2021-07-29 21:42:04 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -4,19 +4,5 @@ function toggleMenu() {
 const btn = document.getElementById('hamburgerBtn');
 btn.onclick = toggleMenu;
 
-function ValidateEmail(inputText) {
-const mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-
-    if(inputText.value.match(mailformat)) {
-        alert("Valid email address!");
-        document.commentForm.eAddress.focus();
-        return true;
-    }
-    else {
-        alert("You have entered an invalid email address!");
-        document.commentForm.eAddress.focus();
-        return false;
-    }
-}
 
 
